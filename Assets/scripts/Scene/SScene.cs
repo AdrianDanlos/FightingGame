e226eb7ad87c09fe2da54b6e05a4900308454daf
JoelEncinas <joelencinasmartin@gmail.com>
2021-10-07 13:28:37 +0200

    project reset

---
+++ Assets/scripts/Scene/SScene.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SScene : MonoBehaviour
+{
+    // stores current scene
+    public static int scene;
+}

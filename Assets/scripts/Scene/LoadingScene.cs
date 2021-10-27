fc4a2dcd5d42029f1f3f34c0564c1baaef10c327
JoelEncinas <joelencinasmartin@gmail.com>
2021-10-27 20:09:55 +0200

    loading scene canvas tips removed as loading is instantaneous

--- Assets/scripts/Scene/LoadingScene.cs
+++ Assets/scripts/Scene/LoadingScene.cs
@@ -8,56 +8,40 @@ public class LoadingScene : MonoBehaviour
 {
     [Header("UI")]
     public UILoading uILoading;
-    [SerializeField] private Image progressBar;
 
     [Header("Scenes")]
     public SceneTransition sceneTransition;
 
-    private IEnumerator Start()
+    private void Start()
     {
         StartCoroutine(sceneTransition.DisplayAnimation());
-        uILoading.LoadRandomTip();
+        // uILoading.LoadRandomTip(); -- unnecessary because loading is almost instantaneous
 
         // create async operation depending from which scene you came 
         switch (SScene.scene)
         {
             // INITIAL_MENU > MAIN_MENU
             case 0:
-                yield return new WaitForSeconds(1f);
-                StartCoroutine(LoadAsyncOperation((int)SceneIndex.MAIN_MENU));
+                SceneManager.LoadScene((int)SceneIndex.MAIN_MENU);
                 break;
             // MAIN_MENU > GAME || MAIN_MENU > INITIAL_MENU
             case 1:
-                yield return new WaitForSeconds(1f);
                 if (SScene.toInitialMenu)
                 {
                     SScene.scene = -1;
-                    StartCoroutine(LoadAsyncOperation((int)SceneIndex.INITIAL_MENU));
+                    SceneManager.LoadScene((int)SceneIndex.INITIAL_MENU);
                 }
                 else if(!SScene.toInitialMenu) {
-                    StartCoroutine(LoadAsyncOperation((int)SceneIndex.GAME));
+                    SceneManager.LoadScene((int)SceneIndex.GAME);
                 }
                 break;
             // GAME > MAIN_MENU
             case 3:
-                yield return new WaitForSeconds(1f);
-                StartCoroutine(LoadAsyncOperation((int)SceneIndex.MAIN_MENU));
+                SceneManager.LoadScene((int)SceneIndex.MAIN_MENU);
                 break;
 
             default:
                 break;
         }        
-    }
-    
-    public IEnumerator LoadAsyncOperation(int sceneNumber)
-    {
-        AsyncOperation loadProgress = SceneManager.LoadSceneAsync(sceneNumber);
-
-        // fill progress bar with the progress of the operation load
-        while (loadProgress.progress < 1)
-        {
-            progressBar.fillAmount = loadProgress.progress;
-            yield return new WaitForEndOfFrame();
-        }
     }
 }

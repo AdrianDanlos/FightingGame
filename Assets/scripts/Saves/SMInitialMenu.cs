3cacf3b49a822ce9dd7304b02884728663c9ab3e
JoelEncinas <joelencinasmartin@gmail.com>
2021-10-28 15:38:49 +0200

    added Leantween dependency

--- Assets/scripts/Saves/SMInitialMenu.cs
+++ Assets/scripts/Saves/SMInitialMenu.cs
@@ -107,8 +107,8 @@ public class SMInitialMenu : MonoBehaviour
     private int[] generateBaseStats()
     {
         int strength, agility, speed;
-        int minValue = 14;
-        int maxValue = 17;
+        int minValue = 4;
+        int maxValue = 7;
         strength = Random.Range(minValue, maxValue);
 
         if (strength == minValue)
---
+++ Assets/scripts/UI/LibraryTransition.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LibraryTransition : MonoBehaviour
+{
+    [Header("Animator")]
+    public Animator transition;
+    public float transitionTime = 1f;
+
+    public IEnumerator DisplayAnimation()
+    {
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime);
+    }
+}
